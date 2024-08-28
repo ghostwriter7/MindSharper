21579cce6534a4818831c4e089082a486d2f5eaf
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-28 18:30:29 +0200

    Core entities added

---
+++ MindSharper.Domain/Entities/Deck.cs
@@ -0,0 +1,10 @@
+﻿namespace MindSharper.Domain.Entities;
+
+public class Deck
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<Flashcard> Flashcards { get; set; } = [];
+    public DateOnly CreatedAt { get; set; } = default;
+    public byte Rate { get; set; }
+}
---
+++ MindSharper.Domain/Entities/Flashcard.cs
@@ -0,0 +1,10 @@
+﻿namespace MindSharper.Domain.Entities;
+
+public class Flashcard
+{
+    public int Id { get; set; }
+    public string Frontside { get; set; } = default!;
+    public string Backside { get; set; } = default!;
+    public DateOnly CreatedAt { get; set; } = default;
+    public DateTime? ReviewedAt { get; set; } 
+}

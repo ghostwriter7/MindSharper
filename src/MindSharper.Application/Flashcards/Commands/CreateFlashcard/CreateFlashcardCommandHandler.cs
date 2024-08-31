8a49df8990d502a2c93703588a770c8756575ea6
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-31 19:22:01 +0200

    Added unique constraint violation handling

--- src/MindSharper.Application/Flashcards/Commands/CreateFlashcard/CreateFlashcardCommandHandler.cs
+++ src/MindSharper.Application/Flashcards/Commands/CreateFlashcard/CreateFlashcardCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using MindSharper.Application.Helpers;
 using MindSharper.Domain.Entities;
 using MindSharper.Domain.Exceptions;
 using MindSharper.Domain.Repositories;
@@ -17,11 +18,18 @@ public class CreateFlashcardCommandHandler(ILogger<CreateFlashcardCommandHandler
         logger.LogInformation($"Creating a {nameof(Flashcard)} for {nameof(Deck)} ({request.DeckId})");
         var deck = await deckRepository.GetDeckByIdAsync(request.DeckId)
                    ?? throw new NotFoundException(nameof(Deck), request.DeckId.ToString());
-
+        
         var flashcard = mapper.Map<Flashcard>(request);
         flashcard.CreatedAt = DateOnly.FromDateTime(DateTime.Now);
 
-        var flashcardId = await flashcardRepository.CreateFlashcardAsync(flashcard);
-        return flashcardId;
+        try
+        {
+            var flashcardId = await flashcardRepository.CreateFlashcardAsync(flashcard);
+            return flashcardId;
+        }
+        catch (Exception ex) when (ExceptionHelper.IsUniqueConstraintViolationException(ex))
+        {
+            throw new DuplicateResourceException(nameof(Flashcard), nameof(Flashcard.Frontside), flashcard.Frontside);
+        }
     }
 }

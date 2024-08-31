d717dbf8e8148ae13649a9312e6a736b56df1f0f
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-31 11:22:10 +0200

    Tests for validators added

---
+++ tests/MindSharper.Application.Tests/Decks/Commands/CreateDeck/CreateDeckCommandValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using JetBrains.Annotations;
+using MindSharper.Application.Decks.Commands.CreateDeck;
+using Xunit;
+
+namespace MindSharper.Application.Tests.Decks.Commands.CreateDeck;
+
+[TestSubject(typeof(CreateDeckCommandValidator))]
+public class CreateDeckCommandValidatorTest
+{
+
+    [Fact]
+    public void Validator_ForValidCommand_ShouldNotHaveAnyValidatorErrors()
+    {
+        var command = new CreateDeckCommand("New name");
+        var validator = new CreateDeckCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldNotHaveAnyValidationErrors();
+    }
+    
+    
+    [Fact]
+    public void Validator_ForNameShorterThan2Chars_ShouldHaveValidationError()
+    {
+        var command = new CreateDeckCommand("A");
+        var validator = new CreateDeckCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldHaveValidationErrorFor(command => command.Name);
+    }
+    
+    
+    [Fact]
+    public void Validator_ForNameLongerThan20Chars_ShouldHaveValidationError()
+    {
+        var command = new CreateDeckCommand(new string(Enumerable.Repeat('a', 21).ToArray()));
+        var validator = new CreateDeckCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldHaveValidationErrorFor(command => command.Name);
+    }
+}
---
+++ tests/MindSharper.Application.Tests/Decks/Commands/UpdateDeckName/UpdateDeckNameCommandValidatorTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using FluentValidation.TestHelper;
+using JetBrains.Annotations;
+using MindSharper.Application.Decks.Commands.UpdateDeckName;
+using Xunit;
+
+namespace MindSharper.Application.Tests.Decks.Commands.UpdateDeckName;
+
+[TestSubject(typeof(UpdateDeckNameCommandValidator))]
+public class UpdateDeckNameCommandValidatorTest
+{
+
+    [Fact]
+    public void Validator_ForValidCommand_ShouldNotHaveAnyValidatorErrors()
+    {
+        var command = new UpdateDeckNameCommand(100, "New name");
+        var validator = new UpdateDeckNameCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldNotHaveAnyValidationErrors();
+    }
+    
+    
+    [Fact]
+    public void Validator_ForNameShorterThan2Chars_ShouldHaveValidationError()
+    {
+        var command = new UpdateDeckNameCommand(100, "A");
+        var validator = new UpdateDeckNameCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldHaveValidationErrorFor(command => command.Name);
+    }
+    
+    
+    [Fact]
+    public void Validator_ForNameLongerThan20Chars_ShouldHaveValidationError()
+    {
+        var command = new UpdateDeckNameCommand(100, new string(Enumerable.Repeat('a', 21).ToArray()));
+        var validator = new UpdateDeckNameCommandValidator();
+
+        var validationResults = validator.TestValidate(command);
+        
+        validationResults.ShouldHaveValidationErrorFor(command => command.Name);
+    }
+}
